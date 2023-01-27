ff705a6d3158c9aef90b6081343324b165a1ccdb
WAMAN BIRAJDAR <78862138+WamanBirajdar@users.noreply.github.com>
2023-01-27 17:45:15 +0530

    Update AddNode.py

--- LinkedList/AddNode.py
+++ LinkedList/AddNode.py
@@ -94,7 +94,90 @@ LL1.printll()
 
 
 
+ # create linked list
+
+class Node:
+    def __init__(self,data) -> None:
+        self.data=data
+        self.ref=None
+
+# linked each node to other node initial linkedlist
+class LinkedList:
+    def __init__(self) -> None:
+        self.head=None
+
+    # traversal list
+    def traversal(self):
+        if self.head is None:
+            print("Linked list is empty")
+        else:
+            n=self.head
+            while n is not None:
+                print(n.data,end="-->")
+                n=n.ref
+
+    # add element in linked list
+    def add_begin(self,data):
+        new_node=Node(data)
+        new_node.ref=self.head
+        self.head=new_node
 
 
+    # add node at d end of linked list
+    def add_end(self,data):
+        new_node=Node(data)
+        if self.head is None:
+            self.head=new_node
+        else:
+            n=self.head
+            while n.ref is not None:
+                n=n.ref
+            n.ref=new_node
+
+
+    # add node after given node
+    def add_after(self,data,x):
+        n=self.head
+
+        while n is not None:
+            if x==n.data:
+                break
+            n=n.ref
+        if n is None:
+            print("\nNode is not present in ll")
+        else:
+            new_node=Node(data)
+            new_node.ref=n.ref
+            n.ref=new_node
+
+    def add_before(self,data,x):
+        if self.head is None:
+            print("Linked list is  empty")
+            return
+
+        if self.head.data==x:
+            new_node=Node(data)
+            new_node.ref=self.head
+            self.head=new_node
+            return
+
+        n=self.head
+
+        while n.ref is not None:
+            if x==n.ref.data:
+                break
+            n=n.ref
+
+        if n.ref is None:
+            print("Node is not found")
+        else:
+            new_node=Node(data)
+            new_node.ref=n.ref
+            n.ref=new_node
+
+
+LL1=LinkedList()
+LL1.add_before(10,100)
+LL1.traversal()
 
 
